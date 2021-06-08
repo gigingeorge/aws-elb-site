61d1fb72b7734fe6cdf231efe3e495e0a67de081
gigingeorge <83015779+gigingeorge@users.noreply.github.com>
2021-06-08 09:33:26 +0530

    commit

--- index.php
+++ index.php
@@ -36,7 +36,7 @@ https://www.tooplate.com/view/2114-pixie
                <?php
 $output = shell_exec('echo $HOSTNAME');
 echo "<h1><center><pre>$output</pre></center></h1>";
-echo "<h1><center>Version changed</center></h1>"
+echo "<h1><center> changed</center></h1>"
 ?>
             </span>
           </div>
